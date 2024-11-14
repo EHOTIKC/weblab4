e67bf892bbc3ae40bf268e2731e408e57d294163
EHOTIKC <dimasaltykov94@gmail.com>
2024-11-14 09:56:58 +0200

    first commit

---
+++ code/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    document.querySelector('.wrapper').classList.toggle('open');
+});
+
+document.querySelector('.close-nav-menu').addEventListener('click',()=>{
+    document.querySelector('.wrapper').classList.toggle('open');
+})
+
+
+// document.querySelector('#open-nav-menu').addEventListener('click',()=>{
+//     document.querySelector('header nav .wrapper').classList.add('nav-open');
+// })
