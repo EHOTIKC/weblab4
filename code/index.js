1a5c08951c864fefa007d860b310f07d8e68da9d
EHOTIKC <dimasaltykov94@gmail.com>
2024-11-20 09:26:12 +0200

    6commit

--- code/index.js
+++ code/index.js
@@ -4,13 +4,13 @@
 
 
 
-document.querySelector('.navbar-toggler').addEventListener('click', () => {
-    document.querySelector('.wrapper').classList.toggle('open');
-});
+// document.querySelector('.navbar-toggler').addEventListener('click', () => {
+//     document.querySelector('.wrapper').classList.toggle('open');
+// });
 
-document.querySelector('.close-nav-menu').addEventListener('click',()=>{
-    document.querySelector('.wrapper').classList.toggle('open');
-})
+// document.querySelector('.close-nav-menu').addEventListener('click',()=>{
+//     document.querySelector('.wrapper').classList.toggle('open');
+// })
 
 
 // document.querySelector('#open-nav-menu').addEventListener('click',()=>{
